0061260688f076fb1e90ee2b5cf290316b578d92
Amit Joshi <amit.joshi314@gmail.com>
2024-08-18 13:14:27 +0530

    04_control_flow modified

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,7 +1,9 @@
+
+// JS will treat empty array, object [], {} as truthy values
 const userEmail = []
 
 if (userEmail) {
-    console.log("Got user email");
+    console.log("Got user email");             // Got user email
 } else {
     console.log("Don't have user email");
 }
@@ -13,29 +15,48 @@ if (userEmail) {
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
 
+
+//++++ To check if array is empty
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
 // }
 
+//++++ To check if object is empty
 const emptyObj = {}
-
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
 
-// Nullish Coalescing Operator (??): null undefined
-
-let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+// ++++ If we do below comparision will get result as true
+//console.log(false==0)                // true
+//console.log(false=='')               // true
+//console.log(0=='')                   // true
 
 
+// Nullish Coalescing Operator (??): null undefined (mainly made for null and undefined)
 
-console.log(val1);
 
-// Terniary Operator
+// let val;
+// val = 5 ?? 10
+// console.log(val)               // 5
+
+
+// let val2;
+// val2 = null ?? 10
+// console.log(val2)              // 10
+
+// let val3;
+// val3 = undefined ?? 15
+// console.log(val3);             // 15
+
+
+// let val4;
+// val4 = null ?? 10 ?? 20
+// console.log(val4);             // 10 // it will take first value
+
+
+
+// +++ Terniary Operator( ? : ) ( dont confuse with Nullish Coalescing Operator (??) as we seen above)
 
 // condition ? true : false
 
